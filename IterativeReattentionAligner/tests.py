c4d2a47d978b269ca8d8d517cba02072dd06349f
ahmed <shinigami1494@github.com>
2019-03-13 17:07:15 -0400

    added code for iterative reattention, not tested

--- IterativeReattentionAligner/tests.py
+++ IterativeReattentionAligner/tests.py
@@ -19,18 +19,18 @@ def test_interactive_aligner(t1, t2, t1_lens, t2_lens):
 
 	m = t1.shape[1]
 	n = t2.shape[1]
-	success |= test_wrapper("checking size of H", t1.shape == H.shape)
-	success |= test_wrapper("checking h_lens", torch.all(t1_lens == h_lens))
-	success |= test_wrapper('checking shape of E', E.shape[0] == t1.shape[0] and
+	success &= test_wrapper("checking size of H", t1.shape == H.shape)
+	success &= test_wrapper("checking h_lens", torch.all(t1_lens == h_lens))
+	success &= test_wrapper('checking shape of E', E.shape[0] == t1.shape[0] and
 										E.shape[1] == t2.shape[1] and
 										E.shape[2] == t1.shape[1])	
 
 	for i in range(len(t1_lens)):
-		success |= test_wrapper("checking masking of cols in batch %d of E" % i, 
+		success &= test_wrapper("checking masking of cols in batch %d of E" % i, 
 			torch.all(E[i, :, t1_lens[i]:] == 0))
 	
 	for i in range(len(h_lens)):
-		success |= test_wrapper("checking masking of rows in batch %d of H" % i, 
+		success &= test_wrapper("checking masking of rows in batch %d of H" % i, 
 			torch.all(H[i, h_lens[i]:] == 0))
 
 	print "test_interactive_aligner...%s\n" % bool(success)
@@ -41,19 +41,24 @@ def test_self_aligner(t, t_lens):
 
 	success = 1
 
-	success |= test_wrapper("checking size of H", t.shape == Ht.shape)
-	success |= test_wrapper("checking h_lens", torch.all(t_lens == ht_lens))
-	success |= test_wrapper('checking shape of E', B.shape[0] == t.shape[0] and
+	success &= test_wrapper("checking size of H", t.shape == Ht.shape)
+	success &= test_wrapper("checking h_lens", torch.all(t_lens == ht_lens))
+	success &= test_wrapper('checking shape of E', B.shape[0] == t.shape[0] and
 										B.shape[1] == t.shape[1] and
 										B.shape[2] == t.shape[1])	
 
 	for i in range(len(t_lens)):
-		success |= test_wrapper("checking masking of cols in batch %d of B" % i, 
+		success &= test_wrapper("checking masking of cols in batch %d of B" % i, 
 			torch.all(B[i, :, t_lens[i]:] == 0))
 	
+	for i in range(B.shape[0]):
+		success &= test_wrapper('checking diagonals of batch %d of B'%i, 
+			torch.all(torch.diag(B[i] == 0)))
+
 	for i in range(len(ht_lens)):
-		success |= test_wrapper("checking masking of rows in batch %d of Ht" % i, 
+		success &= test_wrapper("checking masking of rows in batch %d of Ht" % i, 
 			torch.all(Ht[i, ht_lens[i]:] == 0))
+	
 	print "test_self_aligner...%s\n" % bool(success)
 
 if __name__ == '__main__':
